33cbacc67da803be6edadc15ac76b3f85bda5085
Nathanael <dante2@dante2.info>
2019-01-24 15:33:09 -0700

    changing the directions of the ball

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#ff0000";
-ctx.fill();
+setInterval(draw, 10);
 
-ctx.closePath();
+var x = canvas.width/2;
+var y = canvas.height - 30;
 
-ctx.beginPath();
-ctx.arc(240, 165, 30, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
-ctx.stroke();
-ctx.closePath();
+var ballRadius = 10;
+
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.width)
+    drawBall();
+    
+    if(y + dy < 0){
+        dy = -dy;
+    }
+    if(y + dy > canvas.height){
+        dy = -dy;
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+
